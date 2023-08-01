5d12617a0a5e32171b0efe57aa00be439f5ed9df
filipediaslima <filipdiasdelima@gmail.com>
2023-07-31 23:53:12 -0400

    init project

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:book_tickets/styles/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good Morning", style: Styles.headLineStyle3),
+                        const Gap(5),
+                        Text("Book Tickets", style: Styles.headLineStyle1),
+                      ],
+                    ),
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.fitHeight,
+                              image: AssetImage("assets/images/img_1.png"))),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
